7f4d2137abc7a3314ad91be943e12538844f7057
ff <federico.f.salsero@gmail.com>
2025-01-22 11:07:49 +0100

    hello

---
+++ src/main/java/projekty/hd/HelloController.java
@@ -0,0 +1,13 @@
+package projekty.hd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "hello.html";
+    }
+}
---
+++ src/test/java/projekty/hd/HdApplicationTests.java
@@ -0,0 +1,13 @@
+package projekty.hd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
